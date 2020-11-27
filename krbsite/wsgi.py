f8ba01aca33ca207b5bd29ab8d476535d1579b15
Daniel Carter <danielchriscarter@gmail.com>
2020-11-27 01:18:15 +0000

    Initial commit

---
+++ krbsite/wsgi.py
@@ -0,0 +1,22 @@
+"""
+WSGI config for krbsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+path='/var/www/krbsite'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krbsite.settings')
+
+application = get_wsgi_application()
